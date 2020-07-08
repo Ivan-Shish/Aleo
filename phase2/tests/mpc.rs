61c6b0f1f820577faaa290e3cc6810895d22d770
Georgios Konstantopoulos <me@gakonst.com>
2020-07-08 13:45:02 +0300

    feat: make phase2 work with Aleo circuits

--- phase2/tests/mpc.rs
+++ phase2/tests/mpc.rs
@@ -4,14 +4,25 @@ use snark_utils::{Groth16Params, UseCompression};
 use test_helpers::{setup_verify, TestCircuit};
 
 use rand::{thread_rng, Rng};
-use zexe_algebra::{Bls12_377, Bls12_381, PairingEngine, PrimeField, BW6_761};
-use zexe_groth16::{create_random_proof, prepare_verifying_key, verify_proof, Parameters};
-use zexe_r1cs_core::ConstraintSynthesizer;
+use zexe_algebra::{serialize::CanonicalSerialize, Bls12_377, PairingEngine, BW6_761};
+use zexe_groth16::Parameters;
 
-fn generate_mpc_parameters<E, C>(c: C, rng: &mut impl Rng) -> MPCParameters<E>
+// SnarkOS data types
+use snarkos_algorithms::groth16::{
+    create_random_proof, prepare_verifying_key, verify_proof, Parameters as AleoGroth16Params,
+};
+use snarkos_curves::{bls12_377::Bls12_377 as AleoBls12_377, bw6_761::BW6_761 as AleoBW6};
+use snarkos_models::{
+    curves::PairingEngine as AleoPairingEngine,
+    gadgets::r1cs::{ConstraintCounter, ConstraintSynthesizer},
+};
+use snarkos_utilities::serialize::CanonicalDeserialize;
+
+fn generate_mpc_parameters<Aleo, E, C, R: Rng>(c: C, rng: &mut R) -> MPCParameters<E>
 where
+    Aleo: AleoPairingEngine,
     E: PairingEngine,
-    C: Clone + ConstraintSynthesizer<E::Fr>,
+    C: Clone + ConstraintSynthesizer<Aleo::Fr>,
 {
     let powers = 6; // powers of tau
     let batch = 4;
@@ -36,13 +47,18 @@ where
     groth_params.write(&mut writer, compressed).unwrap();
 
     // perform the MPC on only the amount of constraints required for the circuit
-    let mut counter = zexe_r1cs_std::test_constraint_counter::ConstraintCounter::new();
+    let mut counter = ConstraintCounter::new();
     c.clone().generate_constraints(&mut counter).unwrap();
     let phase2_size = counter.num_aux + counter.num_inputs + counter.num_constraints;
 
-    let mut mpc =
-        MPCParameters::<E>::new_from_buffer(c, writer.as_mut(), compressed, 32, phase2_size)
-            .unwrap();
+    let mut mpc = MPCParameters::<E>::new_from_buffer::<Aleo, _>(
+        c,
+        writer.as_mut(),
+        compressed,
+        32,
+        phase2_size,
+    )
+    .unwrap();
 
     let before = mpc.clone();
     // it is _not_ safe to use it yet, there must be 1 contribution
@@ -55,41 +71,37 @@ where
 
 #[test]
 fn test_groth_bls12_377() {
-    groth_test_curve::<Bls12_377>()
-}
-
-#[test]
-fn test_groth_bls12_381() {
-    groth_test_curve::<Bls12_381>()
+    groth_test_curve::<AleoBls12_377, Bls12_377>()
 }
 
 #[test]
 fn test_groth_bw6() {
-    groth_test_curve::<BW6_761>()
+    groth_test_curve::<AleoBW6, BW6_761>()
 }
 
-fn groth_test_curve<E: PairingEngine>() {
+fn groth_test_curve<Aleo: AleoPairingEngine, E: PairingEngine>() {
     let rng = &mut thread_rng();
     // generate the params
     let params: Parameters<E> = {
-        let c = TestCircuit::<E>(None);
-        let setup = generate_mpc_parameters(c, rng);
+        let c = TestCircuit::<Aleo>(None);
+        let setup = generate_mpc_parameters::<Aleo, E, _, _>(c, rng);
         setup.get_params().clone()
     };
+
+    // convert the Zexe params to snarkOS params
+    let mut v = Vec::new();
+    params.serialize(&mut v).unwrap();
+    let params = AleoGroth16Params::<Aleo>::deserialize(&mut &v[..]).unwrap();
 
     // Prepare the verification key (for proof verification)
     let pvk = prepare_verifying_key(&params.vk);
 
     // Create a proof with these params
     let proof = {
-        let c = TestCircuit::<E>(Some(<E::Fr as PrimeField>::BigInt::from(5).into()));
+        let c = TestCircuit::<Aleo>(Some(Aleo::Fr::from(5)));
         create_random_proof(c, &params, rng).unwrap()
     };
 
-    let res = verify_proof(
-        &pvk,
-        &proof,
-        &[<E::Fr as PrimeField>::BigInt::from(25).into()],
-    );
+    let res = verify_proof(&pvk, &proof, &[Aleo::Fr::from(25u8)]);
     assert!(res.is_ok());
 }
