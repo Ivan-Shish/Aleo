8bd6698e69c5fd025ca3e658e1da66886ea0777b
raychu86 <raychu86@gmail.com>
2020-10-04 03:18:51 -0700

    Fix warp websocket path exposure

--- phase1-verifier/src/main.rs
+++ phase1-verifier/src/main.rs
@@ -22,15 +22,14 @@ use warp::{ws::WebSocket, Filter, Rejection, Reply};
 /// 2. Spawns a tokio task to send a request to the coordinator API endpoints to lock/verify.
 /// 3. Return to step 1.
 ///
-async fn ws_client_connection(ws: WebSocket, id: String) {
+async fn ws_client_connection(ws: WebSocket) {
     let (_client_ws_sender, mut client_ws_rcv) = ws.split();
 
     dotenv::dotenv().ok();
 
     // Fetch the coordinator api url and verifier view key from the `.env` file
     // TODO Remove hardcoded values
-    let coordinator_api_url =
-        env::var("COORDINATOR_API_URL").unwrap_or("http://localhost:8000/api/coordinator".to_string());
+    let coordinator_api_url = env::var("COORDINATOR_API_URL").unwrap_or("http://localhost:8000/api".to_string());
     let view_key = env::var("VIEW_KEY").unwrap_or("AViewKey1cWNDyYMjc9p78PnCderRx37b9pJr4myQqmmPeCfeiLf3".to_string());
 
     let verifier = Verifier::new(coordinator_api_url.to_string(), view_key.to_string()).unwrap();
@@ -46,13 +45,17 @@ async fn ws_client_connection(ws: WebSocket, id: String) {
                     if let Ok(verifier_request) = serde_json::from_str::<VerifierRequest>(&message_string) {
                         if verifier_request.method.to_lowercase() == "lock" {
                             info!("Attempting to lock chunk {:?}", verifier_request.chunk_id);
-
                             // Spawn a task to lock the chunk
                             let verifier_clone = verifier.clone();
                             task::spawn(async move {
+                                // TODO REMOVE THESE SLEEPS
+                                std::thread::sleep(std::time::Duration::from_secs(3));
+
                                 if let Err(err) = verifier_clone.lock_chunk(verifier_request.chunk_id).await {
                                     debug!("Failed to lock chunk (error {})", err);
                                 }
+
+                                info!("Locked chunk {:?}", verifier_request.chunk_id);
                             });
                         } else if verifier_request.method.to_lowercase() == "verify" {
                             info!("Attempting to verify chunk {:?}", verifier_request.chunk_id);
@@ -60,36 +63,37 @@ async fn ws_client_connection(ws: WebSocket, id: String) {
                             // Spawn a task to verify a contribution in the chunk
                             let verifier_clone = verifier.clone();
                             task::spawn(async move {
+                                // TODO REMOVE THESE SLEEPS
+                                std::thread::sleep(std::time::Duration::from_secs(5));
+
                                 if let Err(err) = verifier_clone.verify_contribution(verifier_request.chunk_id).await {
                                     debug!("Failed to verify chunk (error {})", err);
                                 }
+
+                                info!("Verified chunk {:?}", verifier_request.chunk_id);
                             });
                         }
                     }
                 }
             }
             Err(e) => {
-                error!("error receiving ws message for id: {}): {}", id.clone(), e);
-                // TODO re-establish the websocket connection
+                error!("error receiving ws message: {}", e);
                 break;
             }
         };
     }
 }
 
-pub async fn ws_handler(ws: warp::ws::Ws, id: String) -> Result<impl Reply, Rejection> {
-    Ok(ws.on_upgrade(move |socket| ws_client_connection(socket, id)))
+pub async fn ws_handler(ws: warp::ws::Ws) -> Result<impl Reply, Rejection> {
+    Ok(ws.on_upgrade(move |socket| ws_client_connection(socket)))
 }
 
 #[tokio::main]
 async fn main() {
     dotenv::dotenv().ok();
-    init_logger("TRACE");
+    init_logger("INFO");
 
-    let ws_route = warp::path("ws")
-        .and(warp::ws())
-        .and(warp::path::param())
-        .and_then(ws_handler);
+    let ws_route = warp::path("ws").and(warp::ws()).and_then(ws_handler);
 
     // Fetch the coordinator api url and verifier view key from the `.env` file
     let _coordinator_api_url =
