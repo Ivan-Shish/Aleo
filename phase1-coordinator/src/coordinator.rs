3530f8edb0bba7166507b4a678caf37043686163
howardwu <howardwu@berkeley.edu>
2020-10-20 19:53:21 -0700

    Test without spawn

--- phase1-coordinator/src/coordinator.rs
+++ phase1-coordinator/src/coordinator.rs
@@ -305,36 +305,35 @@ impl Coordinator {
                 let verifier = verifier.clone();
                 let seed = seed.clone();
 
-                task::spawn(async move {
-                    fn contribute(
-                        coordinator: &Coordinator,
-                        contributor: &Participant,
-                        seed: &[u8; 32],
-                    ) -> anyhow::Result<()> {
-                        let (chunk_id, _previous_response, _challenge, _response) =
-                            coordinator.try_lock(contributor)?;
+                // task::spawn(async move {
+                fn contribute(
+                    coordinator: &Coordinator,
+                    contributor: &Participant,
+                    seed: &[u8; 32],
+                ) -> anyhow::Result<()> {
+                    let (chunk_id, _previous_response, _challenge, _response) = coordinator.try_lock(contributor)?;
 
-                        debug!("Computing contributions for round 1 chunk {}", chunk_id);
-                        coordinator.run_computation(1, chunk_id, 1, contributor, seed)?;
-                        let _response = coordinator.try_contribute(contributor, chunk_id)?;
-                        debug!("Computed contributions for round 1 chunk {}", chunk_id);
-                        Ok(())
-                    }
+                    debug!("Computing contributions for round 1 chunk {}", chunk_id);
+                    coordinator.run_computation(1, chunk_id, 1, contributor, seed)?;
+                    let _response = coordinator.try_contribute(contributor, chunk_id)?;
+                    debug!("Computed contributions for round 1 chunk {}", chunk_id);
+                    Ok(())
+                }
 
-                    fn verify(coordinator: &Coordinator, verifier: &Participant) -> anyhow::Result<()> {
-                        let (chunk_id, _challenge, _response, _next_challenge) = coordinator.try_lock(&verifier)?;
+                fn verify(coordinator: &Coordinator, verifier: &Participant) -> anyhow::Result<()> {
+                    let (chunk_id, _challenge, _response, _next_challenge) = coordinator.try_lock(&verifier)?;
 
-                        debug!("Running verification for round 1 chunk {}", chunk_id);
-                        let _next_challenge = coordinator.run_verification(1, chunk_id, 1, &verifier)?;
-                        coordinator.try_verify(&verifier, chunk_id)?;
-                        debug!("Running verification for round 1 chunk {}", chunk_id);
-                        Ok(())
-                    }
+                    debug!("Running verification for round 1 chunk {}", chunk_id);
+                    let _next_challenge = coordinator.run_verification(1, chunk_id, 1, &verifier)?;
+                    coordinator.try_verify(&verifier, chunk_id)?;
+                    debug!("Running verification for round 1 chunk {}", chunk_id);
+                    Ok(())
+                }
 
-                    let seed = seed.expose_secret()[..].try_into().expect("Seed failed");
-                    contribute(&coordinator, &contributor, seed).expect("Contribute failed");
-                    verify(&coordinator, &verifier).expect("Verify failed");
-                });
+                let seed = seed.expose_secret()[..].try_into().expect("Seed failed");
+                contribute(&coordinator, &contributor, seed).expect("Contribute failed");
+                verify(&coordinator, &verifier).expect("Verify failed");
+                // });
             }
             info!("Added contributions and verifications for round 1");
         } else {
