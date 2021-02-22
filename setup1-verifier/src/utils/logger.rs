46355b825d5404e4ae9f16bc6ecc696555a73176
ibaryshnikov <baryshnikov.il@gmail.com>
2021-02-22 20:44:55 +0300

    enabled setup1-verifier to read log level from env, updated readme

--- setup1-verifier/src/utils/logger.rs
+++ setup1-verifier/src/utils/logger.rs
@@ -1,22 +1,10 @@
-use tracing::subscriber;
-use tracing_subscriber::{fmt::format::Format, FmtSubscriber};
+use tracing_subscriber::{FmtSubscriber, EnvFilter};
 
-/// Initialize logger with custom format and verbosity.
-pub fn init_logger(verbosity: &str) {
+/// Initialize logger from RUST_LOG environment variable
+pub fn init_logger() {
     let subscriber = FmtSubscriber::builder()
-        // all spans/events with a level higher than TRACE (e.g, debug, info, warn, etc.)
-        // will be written to stdout.
-        .with_max_level(match verbosity {
-            "ERROR" => tracing::Level::ERROR,
-            "WARN" => tracing::Level::WARN,
-            "INFO" => tracing::Level::INFO,
-            "DEBUG" => tracing::Level::DEBUG,
-            "TRACE" => tracing::Level::TRACE,
-            _ => tracing::Level::INFO,
-        })
-        .with_target(false)
-        .event_format(Format::default())
+        .with_env_filter(EnvFilter::from_default_env())
         .finish();
 
-    subscriber::set_global_default(subscriber).expect("setting default subscriber failed");
+    tracing::subscriber::set_global_default(subscriber).expect("setting default subscriber failed");
 }
