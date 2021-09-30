d41ea133d0cada137c2cea116afca1325d3373c2
Jules de Smit <julesdesmit@gmail.com>
2021-09-30 14:13:54 +0200

    Further expand the WASM contributor skeleton

--- phase1-wasm/src/contributor.rs
+++ phase1-wasm/src/contributor.rs
@@ -1,14 +1,53 @@
 use crate::{errors::ContributeError, phase1::Phase1WASM};
 // use snarkvm_dpc::Address;
+use rand::{CryptoRng, Rng};
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
 pub struct Contributor {
     wasm: Phase1WASM,
+    address: String,
+    confirmation_key: String,
 }
 
+#[wasm_bindgen]
 impl Contributor {
-    pub fn contribute() -> Result<String, ContributeError> {
-        Ok("1".to_owned())
+    #[wasm_bindgen(constructor)]
+    pub fn new() -> Self {
+        Self { wasm: Phase1WASM {} }
+    }
+
+    pub async fn contribute(&self) -> Result<JsValue, JsValue> {
+        let mut rng = rand::thread_rng();
+        self.join_queue(&mut rng).await?;
+
+        loop {
+            self.send_heartbeat(&mut rng).await?;
+            self.attempt_contribution(&mut rng).await?;
+
+            if self.is_finished(&mut rng).await? {
+                break;
+            }
+        }
+
+        Ok((self.address, self.confirmation_key))
+    }
+
+    async fn join_queue<R: Rng + CryptoRng>(&self, rng: &mut R) -> Result<bool, ContributeError> {
+        let join_queue_path = "/v1/queue/contributor/join";
+        // TODO: finish
+        unimplemented!();
+    }
+
+    async fn send_heartbeat<R: Rng + CryptoRng>(&self, rng: &mut R) -> Result<(), ContributeError> {
+        unimplemented!();
+    }
+
+    async fn attempt_contribution<R: Rng + CryptoRng>(&self, rng: &mut R) -> Result<(), ContributeError> {
+        unimplemented!();
+    }
+
+    async fn is_finished<R: Rng + CryptoRng>(&self, rng: &mut R) -> Result<bool, ContributeError> {
+        unimplemented!();
     }
 }
--- phase1-wasm/src/errors.rs
+++ phase1-wasm/src/errors.rs
@@ -1,1 +1,13 @@
-pub enum ContributeError {}
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+#[derive(Clone, Copy, Debug)]
+pub enum ContributeError {
+    FailedToJoinQueue,
+}
+
+impl From<ContributeError> for JsValue {
+    fn from(value: ContributeError) -> Self {
+        JsValue::from_str(&format!("{:?}", value))
+    }
+}
