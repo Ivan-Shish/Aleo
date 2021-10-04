246ab8257bb94c8fa2e4094ad6b6de55dc5fc6f9
Jules de Smit <julesdesmit@gmail.com>
2021-10-04 16:41:18 +0200

    Add feature gates and conditional compilation to fix CI

--- phase1-wasm/src/lib.rs
+++ phase1-wasm/src/lib.rs
@@ -1,11 +1,16 @@
 #[macro_use]
 extern crate serde_derive;
 
+#[cfg(feature = "wasm")]
 mod contributor;
+#[cfg(feature = "wasm")]
 mod errors;
 mod phase1;
+#[cfg(feature = "wasm")]
 mod requests;
+#[cfg(feature = "wasm")]
 mod structures;
+#[cfg(feature = "wasm")]
 mod utils;
 
 #[cfg(test)]
