a8dafcd99e39d5d6e54928e229eaf429fe17e35f
Jules de Smit <julesdesmit@gmail.com>
2021-10-09 16:36:30 +0200

    Move error message to correct conditional branch

--- setup1-contributor/src/commands/contribute.rs
+++ setup1-contributor/src/commands/contribute.rs
@@ -99,6 +99,8 @@ impl Contribute {
                 Ok(joined) => {
                     info!("Attempted to join the queue with response: {}", joined);
                     if !joined {
+                        println!("Failed to join the queue - reliability score too low");
+
                         // it means contributor either already contributed,
                         // or has a low reliability score, or unable to
                         // join the queue
@@ -110,7 +112,6 @@ impl Contribute {
                 Err(err) => {
                     let text = format!("Failed to join the queue, error: {}", err);
                     error!("{}", text);
-                    println!("{}", text);
                     sleep(DELAY_POLL_CEREMONY).await;
                 }
             }
